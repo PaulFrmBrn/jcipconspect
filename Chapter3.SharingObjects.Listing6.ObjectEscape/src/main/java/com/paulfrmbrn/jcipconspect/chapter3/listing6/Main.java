3ee20e736c924653bc54904b3619be3d2c60bb5c
paulfrmbrn <lesspaul@bk.ru>
2015-07-14 10:27:47 +0300

    chapter 3 listring 6 excape

---
+++ Chapter3.SharingObjects.Listing6.ObjectEscape/src/main/java/com/paulfrmbrn/jcipconspect/chapter3/listing6/Main.java
@@ -0,0 +1,16 @@
+package com.paulfrmbrn.jcipconspect.chapter3.listing6;
+
+/**
+ * BAD
+ * Allowing internal mutable state to escape
+ *
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, world!");
+
+    }
+
+}

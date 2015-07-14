73bf0436de49afc2db1f040fb703d662c236b24c
paulfrmbrn <lesspaul@bk.ru>
2015-07-14 10:31:32 +0300

    chapter 3 listing 6 object excape

--- Chapter3.SharingObjects.Listing6.ObjectEscape/src/main/java/com/paulfrmbrn/jcipconspect/chapter3/listing6/Main.java
+++ Chapter3.SharingObjects.Listing6.ObjectEscape/src/main/java/com/paulfrmbrn/jcipconspect/chapter3/listing6/Main.java
@@ -1,5 +1,8 @@
 package com.paulfrmbrn.jcipconspect.chapter3.listing6;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * BAD
  * Allowing internal mutable state to escape
@@ -10,6 +13,8 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello, world!");
+
+        Set<Object> object = new HashSet<>();
 
     }
 

dd2818b89d051b2dc3bf45f277b3eea4d2c60ec1
paulfrmbrn <lesspaul@bk.ru>
2015-07-14 10:45:25 +0300

    chapter 3 listing 6 object excape

--- Chapter3.SharingObjects.Listing6.ObjectEscape/src/main/java/com/paulfrmbrn/jcipconspect/chapter3/listing6/Main.java
+++ Chapter3.SharingObjects.Listing6.ObjectEscape/src/main/java/com/paulfrmbrn/jcipconspect/chapter3/listing6/Main.java
@@ -1,20 +1,26 @@
 package com.paulfrmbrn.jcipconspect.chapter3.listing6;
 
-import java.util.HashSet;
-import java.util.Set;
-
 /**
  * BAD
  * Allowing internal mutable state to escape
  *
+ * @author paulfrmbrn
  */
 public class Main {
+
+    private String[] states = new String[]{"AK", "AL"}; // while this object was defined private
+
+    public String[] getStates() { // this method made states[] efffectively public. This is object escape
+        return states;
+    }
 
     public static void main(String[] args) {
 
         System.out.println("Hello, world!");
 
-        Set<Object> object = new HashSet<>();
+        Main main = new Main();
+        main.getStates();
+
 
     }
 

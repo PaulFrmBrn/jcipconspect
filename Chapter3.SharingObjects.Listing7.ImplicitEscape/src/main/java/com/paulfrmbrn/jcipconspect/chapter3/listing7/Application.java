a583d1b3cf5a3afada37a2b60b69c7124606e437
paulfrmbrn <lesspaul@bk.ru>
2015-07-17 10:52:10 +0300

    Chapter 3 Listing 7

--- Chapter3.SharingObjects.Listing7.ImplicitEscape/src/main/java/com/paulfrmbrn/jcipconspect/chapter3/listing7/Application.java
+++ Chapter3.SharingObjects.Listing7.ImplicitEscape/src/main/java/com/paulfrmbrn/jcipconspect/chapter3/listing7/Application.java
@@ -1,7 +1,8 @@
 package com.paulfrmbrn.jcipconspect.chapter3.listing7;
 
 /**
- *
+ * BAD
+ * Implicitly allwoing the this reference to secape
  *
  * @author paulfrmbrn
  */
@@ -11,6 +12,48 @@ public class Application {
 
         System.out.println("Hello, World!");
 
+        EventSource source = new EventSource();
+        new ThisEscape(source);
+
     }
 
+}
+
+class ThisEscape { // enclosing object
+
+    public ThisEscape(EventSource source) {
+
+        source.registerListener( // EventSource have hidden reference to the ThisEscape object
+
+                new EventListener() { // inner class
+
+                    public void onEvent(Event e) {
+                        /*ThisEscape.this.*/doSomething(e); // hidden reference to the enclosing object
+                        // implicit publishing of this reference to the enclosing object (ThisEscape)
+                        // in anonumouse class (EventListener)
+
+                    }
+                }
+        );
+    }
+
+    private void doSomething(Event e) {
+
+    }
+
+
+}
+
+class Event {
+}
+
+abstract class EventListener {
+    abstract public void onEvent(Event e);
+}
+
+class EventSource {
+
+    public void registerListener(EventListener eventListener) {
+        System.out.println("listener registered: " + eventListener);
+    }
 }
